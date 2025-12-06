68fdeaeef7ca3c87a342d5f4636c9cd6a5f4f457
anOticed <notanoticed@gmail.com>
2025-12-06 20:07:43 +0100

    Bump version to 0.3.7.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 30
         targetSdk = 36
         versionCode = 1
-        versionName = "0.3.7.3"
+        versionName = "0.3.7.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
