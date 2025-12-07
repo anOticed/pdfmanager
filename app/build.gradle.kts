3b5acb28754075dbc67ba1b7fb5ba047600f82de
anOticed <notanoticed@gmail.com>
2025-12-07 01:04:52 +0100

    Bump version to 0.3.8 and enable buildConfig

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 30
         targetSdk = 36
         versionCode = 1
-        versionName = "0.3.7.5"
+        versionName = "0.3.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -38,6 +38,7 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
 }
 
