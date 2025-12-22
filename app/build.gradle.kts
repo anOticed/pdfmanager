d29ba53321569495cf44d245b4d4a65ef0e07326
anOticed <notanoticed@gmail.com>
2025-12-22 03:31:31 +0100

    Bump version to 0.4.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 30
         targetSdk = 36
         versionCode = 1
-        versionName = "0.4.1"
+        versionName = "0.4.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
