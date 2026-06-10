2c05618c070dafad2b686bb84b82a3d5e98ddf23
anOticed <notanoticed@gmail.com>
2026-06-10 14:42:39 +0200

    Bump version to 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 31
         targetSdk = 36
         versionCode = 1
-        versionName = "0.9.1"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
